beb013e6528a120945941dca7cef8746183e3cd6
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2018-02-23 00:13:05 -0800

    Minor refactoring and removal of unimplemented devices

--- ssi-65-avionics/ssi-65-avionics.ino
+++ ssi-65-avionics/ssi-65-avionics.ino
@@ -5,12 +5,11 @@
 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
-#include <Adafruit_MAX31855.h>
 #include <IridiumSBD.h>
 #include <TinyGPS++.h>
 #include <SD.h>
 #include <SPI.h>
-#include <Wire.h>
+#include <Wire.h> 
 
 #define DEBUG // comment out to turn off debugging
 #ifdef DEBUG
@@ -21,6 +20,18 @@
 #define DEBUG_PRINTLN(x)
 #endif
 
+// System Parameters
+#define SERIAL_TIMEOUT 60
+#define ROCKBLOCK_ENABLED true
+
+// Environemental Parameters
+#define LAUNCH_SITE_PRESSURE 1014.562
+
+// SPI Ports
+#define SCK_PIN 13
+#define SD_READER_CS 10
+
+// State
 bool firstSend = true;
 String dataStringBuffer = "";
 
@@ -32,19 +43,11 @@ long refBottom; // Time difference between 'b' command and start of program
 #define ROCKBLOCK_TRANSMIT_TIME 300000 // 5 Minutes
 #define BAROMETER_MEASURMENT_INTERVAL 10000 // 10 Seconds
 
-// Environemental Parameters
-#define LAUNCH_SITE_PRESSURE 1014.562
-
-// SPI Ports
-#define SCK_PIN 13
-#define SD_READER_CS 10
-#define THERMOCOUPLE_CS 15
-
 // SD Card Reader (SPI)
 File dataFile;
 long lastFlush;
 
-// BMP280 (I2C) | Pressure, Internal Temperature
+// BMP280 (I2C)
 Adafruit_BMP280 bmp;
 long lastAscentTime;             // Time of last ascent rate calculation
 double lastAlt;                 // Last altitude, for calculation
@@ -57,65 +60,40 @@ IridiumSBD modem(IridiumSerial);
 
 // GPS (Hardware Serial) GPS
 TinyGPSPlus gps;
-float f_lat = 0, f_long = 0;
+float f_lat = 0, f_long = 0, f_alt = 0;
 int sats = -1;
-long unsigned f_age = 0;
-
-// FET pin assignments
-const int WIRE_TOP = 23;
-const int WIRE_BOTTOM = 22;
-const int heater = 2;
-bool applyHeat = false;
-bool releaseTop = false; 
-bool releaseBottom = false;
-
-//Time(seconds) for nichrome to cut through
-const int delayTime = 10;
-
-#define LED_PIN 13
 
 void setup() {
   startTime = millis();
   lastFlush = 0;
   lastTransmit = 0;
 
-  // set FET gates to LOW
-  pinMode(WIRE_TOP, OUTPUT);
-  pinMode(WIRE_BOTTOM, OUTPUT);
-  pinMode(heater, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(WIRE_TOP, LOW);
-  digitalWrite(WIRE_BOTTOM, LOW);
-  digitalWrite(heater, LOW);
-  digitalWrite(LED_PIN, LOW);
-
-#ifdef DEBUG
+  #ifdef DEBUG
   Serial.begin(9600);
   while (!Serial) {
     continue;
   }
-#endif
+  #endif
 
-  // SD Card Reader
+  // SD Card Reader Setup (SPI)
   SPI.setSCK(SCK_PIN);
   pinMode(SD_READER_CS, OUTPUT);
   DEBUG_PRINTLN("Initializing SD card...");
   if (!SD.begin(SD_READER_CS)) {
     DEBUG_PRINTLN("Card failed, or not present");
-    flashLED();
   }
   dataFile = SD.open("datalog.txt", FILE_WRITE);
   DEBUG_PRINTLN("Card initialized.");
 
+  // BMP280 Setup (I2C)
   if (!bmp.begin()) {
     DEBUG_PRINTLN("Could not find a valid BMP280 sensor, check wiring!");
-    flashLED();
   }
   lastAlt = bmp.readAltitude(LAUNCH_SITE_PRESSURE); // initialize ascent rate variables
   ascentRate = 0;
   lastAscentTime = millis();
 
-  // GPS
+  // GPS Setup (Serial)
   Serial1.begin(9600);
 
   // Data column headers. 
@@ -198,6 +176,8 @@ void loop() {
 }
 
 // Reads from all of the sensors and outputs the data string
+// Data string has the format: "Time(ms), Pressure(Pa), Alt(m), AscentRate(m/s), TempIn(C),
+// "GPSLat, GPSLong, GPSAlt. GPSSats"
 String readSensors() {
   String dataString = "";
 
@@ -210,7 +190,6 @@ String readSensors() {
   // BMP280 (Barometer + Thermometer) Input
   DEBUG_PRINTLN("BMP280 stuff");
   double tempIn = bmp.readTemperature();
-  applyHeat = (tempIn < 0);
   double pressure = bmp.readPressure();
   double alt = bmp.readAltitude(LAUNCH_SITE_PRESSURE); // avg sea level pressure for hollister for past month
   if (loopTime - lastAscentTime > BAROMETER_MEASURMENT_INTERVAL) { // calculate ascent rate in m/s every 10s
@@ -235,29 +214,25 @@ String readSensors() {
   while (Serial1.available()) {
     char c = Serial1.read();
     if(gps.encode(c)) {
-      Serial.println("Printing the encoded data!");
+      DEBUG_PRINTLN("Printing the encoded data!");
     }
   }
   f_lat = gps.location.lat();
   f_long = gps.location.lng();
-  f_age = gps.location.age();
+  f_alt = gps.altitude.meters();
   sats = gps.satellites.value();
 
   dataString += String(f_lat) + ", " + String(f_long) + ", ";
-  dataString += String(f_age) + ", " + String(sats) + ", ";
+  dataString += String(f_alt) + ", " + String(sats) + ", ";
   DEBUG_PRINT("GPS Lat: ");
   DEBUG_PRINTLN(f_lat);
   DEBUG_PRINT("GPS Long: ");
   DEBUG_PRINTLN(f_long);
-  DEBUG_PRINT("GPS Age: ");
-  DEBUG_PRINTLN(f_age);
+  DEBUG_PRINT("GPS Alt: ");
+  DEBUG_PRINTLN(f_alt);
   DEBUG_PRINT("GPS Sats: ");
   DEBUG_PRINTLN(sats);
   DEBUG_PRINTLN("");
-
-  int signalQuality = -1;
-  modem.getSignalQuality(signalQuality);
-  dataString += String(signalQuality) + ", ";
 
   dataStringBuffer = dataString;
   return dataString;     
@@ -275,13 +250,6 @@ void flashLED() {
 bool ISBDCallback() {
   String dataString = readSensors();
   long loopTime = millis();
-
-  // heat if too cold
-  if (applyHeat) {
-    digitalWrite(heater, HIGH);
-  } else {
-    digitalWrite(heater, LOW);
-  }
 
   if (dataFile) {
     DEBUG_PRINTLN("Writing to datalog.txt");
